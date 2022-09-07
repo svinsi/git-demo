d4103a19bb6308c574ad3c2ea623832ab525db39
Sviatoslav Oliiarnyk <113034407+svinsi@users.noreply.github.com>
2022-09-07 20:04:39 +0300

    Update loop.sh
    Add TASK

--- loop.sh
+++ loop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# TASK
+# - create a script that does the following:
+# accepts any number of int arguments
+# outputs the sum of all arguments
+# outputs the number of arguments
+# outputs the average of all arguments
+
 for i do
  SUM=$(expr $SUM + $i)  #Sum Elements
  AVG=$(expr $SUM / $i)  #Average value
